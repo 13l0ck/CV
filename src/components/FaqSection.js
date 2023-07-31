f6e7d410aaa434792d6889d288f40ee518b71264
Block <block@to>
2023-07-31 16:26:43 +0500

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+        <h2>Any Questions <span>FAQ</span></h2>
+        <div className='question'>
+            <h4>How do i start?</h4>
+            <div className='answer'>
+                <p>lorem ipsum dolar ist amet.</p>
+                <p>lorem aqsdfjkzxvc asd;fiojgasdjiofg asdklfasl;kdf;ljk</p>
+            </div>
+        </div>
+        <div className='question'>
+            <h4>Daily Schedule?</h4>
+            <div className='answer'>
+                <p>lorem ipsum dolar ist amet.</p>
+                <p>lorem aqsdfjkzxvc asd;fiojgasdjiofg asdklfasl;kdf;ljk</p>
+            </div>
+        </div>
+        <div className='question'>
+            <h4>Different Payment Methods?</h4>
+            <div className='answer'>
+                <p>lorem ipsum dolar ist amet.</p>
+                <p>lorem aqsdfjkzxvc asd;fiojgasdjiofg asdklfasl;kdf;ljk</p>
+            </div>
+        </div>
+        <div className='question'>
+            <h4>What Products do your offer?</h4>
+            <div className='answer'>
+                <p>lorem ipsum dolar ist amet.</p>
+                <p>lorem aqsdfjkzxvc asd;fiojgasdjiofg asdklfasl;kdf;ljk</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FaqSection
